bb8fdc99129c02b50cacaefac0061fcc0bf7a2a0
alexahs <alexander.harold.sexton@gmail.com>
2019-10-18 19:27:38 +0200

    image compress working

---
+++ Assignment_2/svd.py
@@ -0,0 +1,83 @@
+import numpy as np
+from imageio import imread
+import matplotlib.pyplot as plt
+import os
+
+
+
+def rgb2gray(im):
+    return np.dot(im[...,:3], [0.2989, 0.5870, 0.1140])
+
+
+def compression_ratio(m, n, r):
+    pass
+
+
+def preprocess_images(dir, image_names):
+
+    #load images
+    images = np.array([imread(dir + im) for im in image_names])
+
+    #convert to gray scale
+    images_gray = np.array([rgb2gray(images[i]) for i in range(len(images))])
+
+    #scale to floats in [0, 1]
+    images_gray /= 255.0
+
+    return images_gray
+
+
+def compress(A, r):
+    """
+    A = (m, n)
+    U - (m, m)
+    S - (m, n)
+    VT - (n, n)
+
+
+    A = (m, n)
+    U = (m, q)
+    S = (q, q)
+    VT = (q, n)
+
+    """
+
+    m, n = A.shape
+    q = m - r
+    U, S, VT = np.linalg.svd(A)
+
+    U = U[:, :q]
+    S = S[:q]
+    VT = VT[:q, :]
+
+    S_full = np.zeros((q, q))
+
+    for i in range(q):
+        S_full[i, i] = S[i]
+
+    A_compressed = U @ S_full @ VT
+
+    return A_compressed
+
+
+def main():
+
+    dir = 'pics/'
+    image_names = os.listdir(dir)
+    images = preprocess_images(dir, image_names)
+
+    r = 600
+
+    plt.imshow(images[2])
+    plt.show()
+
+    im_reduced = compress(images[2], r)
+
+    plt.imshow(im_reduced)
+    plt.show()
+
+
+
+
+if __name__ == '__main__':
+    main()
